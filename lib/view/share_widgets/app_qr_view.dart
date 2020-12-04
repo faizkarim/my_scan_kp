81d1757491cb4322b1bb7c387be2ee32827b780a
faizkarim <faizkarim@raudhahpay.com>
2020-12-04 20:51:52 +0800

    qr code scanner design page

---
+++ lib/view/share_widgets/app_qr_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRViewPage extends StatefulWidget {
+  @override
+  _QRViewPageState createState() => _QRViewPageState();
+}
+
+class _QRViewPageState extends State<QRViewPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  var qrText = "";
+
+  QRViewController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return QRView(
+      overlay: QrScannerOverlayShape(
+        borderColor: Colors.white,
+        borderLength: 30,
+        borderWidth: 10,
+        cutOutSize: 300,
+      ),
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
--- lib/view/share_widgets/home_menu_button.dart
+++ lib/view/share_widgets/home_menu_button.dart
@@ -2,6 +2,7 @@ import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:my_scan_kp/utils/app_colors.dart';
 import 'package:my_scan_kp/utils/app_size.dart';
+import 'package:my_scan_kp/view/screens/qr_scan_page.dart';
 
 class HomeMenuButton extends StatelessWidget {
   final int index;
@@ -26,64 +27,70 @@ class HomeMenuButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shadowColor: colors[index],
-      margin: EdgeInsets.all(8.0),
-      color: colors[index],
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: AppSize.widthScreen(context),
-            padding: EdgeInsets.all(
-              15.0,
-            ),
-            decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.3),
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(40),
-                )),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  textMenu[index],
-                  style: TextStyle(
-                    color: AppColors.whiteColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: AppSize.largeText,
-                  ),
-                ),
-                Text(
-                  textKertas,
-                  style: TextStyle(
-                    color: AppColors.whiteColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: AppSize.largeText,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(AppSize.spaceX1),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: Icon(
-                      EvaIcons.arrowCircleRight,
-                      color: AppColors.whiteColor.withOpacity(0.6),
-                      size: AppSize.spaceX4,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => QrScanPage()));
+      },
+      child: Card(
+        shadowColor: colors[index],
+        margin: EdgeInsets.all(8.0),
+        color: colors[index],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: AppSize.widthScreen(context),
+              padding: EdgeInsets.all(
+                15.0,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.3),
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(40),
+                  )),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    textMenu[index],
+                    style: TextStyle(
+                      color: AppColors.whiteColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: AppSize.largeText,
                     ),
                   ),
-                )
-              ],
+                  Text(
+                    textKertas,
+                    style: TextStyle(
+                      color: AppColors.whiteColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: AppSize.largeText,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(AppSize.spaceX1),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Icon(
+                        EvaIcons.arrowCircleRight,
+                        color: AppColors.whiteColor.withOpacity(0.6),
+                        size: AppSize.spaceX4,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
